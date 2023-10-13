793485e8f2cbc16f9d520c8e936b759d582fbc92
Flavio Ortigoza <nicolas.flavio75@gmail.com>
2023-10-13 16:58:29 -0300

    Meal Screen

--- meal_app/lib/screens/meals.dart
+++ meal_app/lib/screens/meals.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
 
 class MealsScreen extends StatelessWidget {
-  const MealsScreen({super.key});
+  const MealsScreen({super.key, required this.title, required this.meals});
 
+  final String title;
+  final List<Meal> meals;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: meals.isNotEmpty
+            ? ListView.builder(
+                itemCount: meals.length,
+                itemBuilder: (context, index) {
+                  final meal = meals[index];
+                  //Lista de Comidas por Categoria
+                  return ListTile(
+                    title: Text(meal.title),
+                  );
+                },
+              )
+            : const Center(
+                child: Text('No se encontraron comida de este tipo'),
+              ));
   }
 }
