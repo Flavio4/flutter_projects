fca313e238934e11bf092490ae21e481610f787b
Flavio Ortigoza <nicolas.flavio75@gmail.com>
2023-10-13 16:44:32 -0300

    Meal_app

---
+++ meal_app/lib/screens/categories.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Elige el tipo de comida'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate:
+            //Crea una grid de 2 columnas
+            const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
+        ],
+      ),
+    );
+  }
+}
